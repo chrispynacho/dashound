6f09ca99272c3c5dd4918c65fe772ec11052e3e9
Chris Camacho <ccamacho@peer1.com>
2014-04-30 00:46:42 -0700

    Updated GET /wallboards test

--- test/wallboards.test.js
+++ test/wallboards.test.js
@@ -1,11 +1,17 @@
 var should = require('should');
-
+var supertest = require('supertest');
+var app = require('../app.js');
 
 describe('wallboards API', function() {
-	
-	it('should list wallboards', function(done) {
-		// wallboards.list()
-		done();
-	});
-	
+	describe('GET /wallboards', function() {
+    it('should list wallboards', function(done) {
+      supertest('http://localhost:3000')
+        .get('/wallboards')
+        .expect(200)
+        .end(function(err, res) {
+          res.text.should.eql('wallboards! wooo!');
+          done()
+        });
+    });
+  });
 });
